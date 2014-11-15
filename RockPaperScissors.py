d156275ae8d7665bce2de280c6eacefc4e2ee7b5
zachpanz88 <zachary@panzarino.com>
2014-11-14 20:53:19 -0500

    Game

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,30 @@
 from random import randint
 
-def user_moves():
-    user_legit = false
-    while user_legit = false
-    user_move = input ("What is your move?")
-    if user_move == "rock" or user_move == "paper" or user_move == "scissors":
-        user_legit = true
-    else:
-        user_legit = false
-def cpu_moves():
+user_move = None
+cpu_move = None
+def user_moves(user_move):
+    user_legit = False
+    while user_legit == False:
+        user_move = input ("What is your move?")
+        if user_move == "rock" or user_move == "paper" or user_move == "scissors":
+            user_legit = True
+        else:
+            user_legit = False
+def cpu_moves(cpu_move):
     cpu_num = randint(1,3)
-    cpu_move = None
     if cpu_num == 1:
         cpu_move = "rock"
     elif cpu_num == 2:
         cpu_move = "paper"
     elif cpu_num == 3:
         cpu_move = "scissors"
+game = True
+legit = False
+while game:
+    while legit == False:
+        user_moves()
+        cpu_moves()
+        print (user_move)
+        print (cpu_move)
+        if user_moves == cpu_moves:
+            pass
